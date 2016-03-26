392c3a054f606467b129883ba8546dcbf7531d32
Aleksandr Zelenin <aleksandr@zelenin.me>
2016-03-27 02:50:52 +0300

    migration to random-compat. drop ramsey/uuid

--- Infrastructure/Service/IdentityGenerator/RandomUuidGenerator.php
+++ Infrastructure/Service/IdentityGenerator/RandomUuidGenerator.php
@@ -2,7 +2,6 @@
 
 namespace Zelenin\Ddd\Core\Infrastructure\Service\IdentityGenerator;
 
-use Ramsey\Uuid\UuidFactory;
 use Zelenin\Ddd\Core\Domain\Object\DefaultObject;
 use Zelenin\Ddd\Core\Domain\Service\IdentityGenerator\IdentityGenerator;
 use Zelenin\Ddd\Core\Domain\Service\Service;
@@ -10,20 +9,27 @@ use Zelenin\Ddd\Core\Domain\Service\Service;
 final class RandomUuidGenerator extends DefaultObject implements IdentityGenerator, Service
 {
     /**
-     * @var UuidFactory
-     */
-    private $factory;
-
-    public function __construct()
-    {
-        $this->factory = new UuidFactory();
-    }
-
-    /**
      * @return string
      */
     public function generate()
     {
-        return $this->factory->uuid4()->toString();
+        $bytes = random_bytes(16);
+        $bytes[6] = chr((ord($bytes[6]) & 0x0f) | 0x40);
+        $bytes[8] = chr((ord($bytes[8]) & 0x3f) | 0x80);
+        $hex = bin2hex($bytes);
+
+        $fields = [
+            'time_low' => substr($hex, 0, 8),
+            'time_mid' => substr($hex, 8, 4),
+            'time_hi_and_version' => substr($hex, 12, 4),
+            'clock_seq_hi_and_reserved' => substr($hex, 16, 2),
+            'clock_seq_low' => substr($hex, 18, 2),
+            'node' => substr($hex, 20, 12),
+        ];
+
+        return vsprintf(
+            '%08s-%04s-%04s-%02s%02s-%012s',
+            $fields
+        );
     }
 }
